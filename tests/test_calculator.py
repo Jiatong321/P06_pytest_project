7baf994096051eb840fdb62b2722fda7eec5537c
Jiatong321 <23009822@myrp.edu.sg>
2024-05-27 14:05:16 +0800

    initial commit

---
+++ tests/test_calculator.py
@@ -0,0 +1,84 @@
+from calculator.calculator import Calculator
+
+class TestCalculator:
+    def test_add(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.add(a, b)
+
+        # assert
+        expected = 5555
+        assert result == expected
+
+    def test_add_negative(self):
+        # arrange
+        a = -2
+        b = -2
+        cal = Calculator()
+
+        # act
+        result = cal.add(a, b)
+
+        # assert
+        expected = -4
+        assert result == expected    
+
+    def test_subtract(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.subtract(a, b)
+
+        # assert
+        expected = 3087
+        assert result == expected
+
+    def test_multiply(self):
+        # arrange
+        a = 5
+        b = 5
+        cal = Calculator()
+
+        # act
+        result = cal.multiply(a, b)
+
+        # assert
+        expected = 25
+        assert result == expected
+
+    def test_divide(self):
+        # arrange
+        a = 5
+        b = 5
+        cal = Calculator()
+
+        # act
+        result = cal.divide(a, b)
+
+        # assert
+        expected = 1
+        assert result == expected
+
+    def test_divide_0(self):
+        # arrange
+        a = 5
+        b = 0
+        cal = Calculator()
+
+        # act
+        result = cal.divide(a, b)
+
+        # assert
+        
+        expected =  ZeroDivisionError("Division by zero error")
+        assert result == expected
+        
+    
+    
